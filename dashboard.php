255029113c85e667dc7489b8c0caa03567a98227
mahin678 <mahintazuar678@gmail.com>
2024-03-30 01:25:23 +0600

    user dashboard development

--- dashboard.php
+++ dashboard.php
@@ -10,17 +10,36 @@
             <div class="">
                 <div class="d-flex justify-content-between dashboard_top_content align-items-center">
                     <div class="dashboard_top_item d-flex align-items-center justify-content-center w-100">
-                        <svg viewBox="0 0 24 24" width="60" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g  stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>/svg/ic-leave</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="ic-leave" fill-rule="nonzero" fill="#4A4A4A"> <path d="M12.5260077,13.6408061 L13.8189009,12.3479129 C14.2094252,11.9573886 14.8425902,11.9573886 15.2331145,12.3479129 C15.6236388,12.7384372 15.6236388,13.3716022 15.2331145,13.7621265 L12.5260077,16.4692333 L12.5260077,18.7000011 C14.9846195,18.2451412 16.8432958,16.1444471 16.8432958,13.6216947 C16.8432958,10.7666114 11.5260077,4.43460257 11.5260077,4.43460257 C11.5260077,4.43460257 6.20871966,10.7666114 6.20871966,13.6216947 C6.20871966,16.1444471 8.06739598,18.2451412 10.5260077,18.7000011 L10.5260077,11.053522 C10.5260077,10.5020644 10.9698725,10.0550197 11.5260077,10.0550197 C12.0782925,10.0550197 12.5260077,10.5025091 12.5260077,11.053522 L12.5260077,13.6408061 Z M12.5260077,20.9353298 L12.5260077,22.0565174 C12.5260077,22.607975 12.0821429,23.0550197 11.5260077,23.0550197 C10.973723,23.0550197 10.5260077,22.6075303 10.5260077,22.0565174 L10.5260077,20.9353298 C6.852338,20.456505 4,17.3762618 4,13.6216947 C4,12.1807022 4.66724137,10.5545272 5.84150189,8.58204893 C6.20133043,7.9776226 6.60514012,7.35141081 7.04538055,6.7089653 C7.6552388,5.81899548 8.30494601,4.94064504 8.95448915,4.10901882 C9.18189237,3.81786883 9.39349296,3.55277787 9.58418937,3.31837625 C9.70068596,3.17518011 9.78585715,3.07223094 9.83456994,3.01422207 L11.5260077,1 L13.2174455,3.01422207 C13.2661583,3.07223094 13.3513295,3.17518011 13.4678261,3.31837625 C13.6585225,3.55277787 13.8701231,3.81786883 14.0975263,4.10901882 C14.7470694,4.94064504 15.3967766,5.81899548 16.0066349,6.7089653 C16.4468753,7.35141081 16.850685,7.9776226 17.2105135,8.58204893 C18.3847741,10.5545272 19.0520154,12.1807022 19.0520154,13.6216947 C19.0520154,17.3762618 16.1996774,20.456505 12.5260077,20.9353298 Z" id="leave"> </path> </g> </g> </g></svg>
-                        Total Casual Leave
+                        <?php 
+                            echo file_get_contents('./assets/casual.svg');
+                        ?>
+                        <div>
+                            <div>12</div>
+                            Total Casual Leave
+                        </div>
                     </div>
                     <div class="dashboard_top_item d-flex align-items-center justify-content-center w-100">
-                        <svg viewBox="0 0 24 24" width="60" fill="none" xmlns="http://www.w3.org/2000/svg"><g  stroke-width="0"></g><g  stroke-linejoin="round"></g><g > <circle cx="10" cy="6" r="4" stroke="#1C274C" stroke-width="1.5"></circle> <path d="M18.0429 12.3656L18.4865 11.7609L18.4865 11.7609L18.0429 12.3656ZM19 8.69135L18.4813 9.23307C18.7713 9.51077 19.2287 9.51077 19.5187 9.23307L19 8.69135ZM19.9571 12.3656L19.5135 11.7609L19.5135 11.7609L19.9571 12.3656ZM19 12.8276L19 13.5776H19L19 12.8276ZM18.4865 11.7609C18.0686 11.4542 17.6081 11.0712 17.2595 10.6681C16.8912 10.2423 16.75 9.91131 16.75 9.69673H15.25C15.25 10.4666 15.6912 11.1479 16.1249 11.6493C16.5782 12.1735 17.1391 12.6327 17.5992 12.9703L18.4865 11.7609ZM16.75 9.69673C16.75 9.12068 17.0126 8.87002 17.2419 8.78964C17.4922 8.70189 17.9558 8.72986 18.4813 9.23307L19.5187 8.14963C18.6943 7.36028 17.6579 7.05432 16.7457 7.3741C15.8125 7.70123 15.25 8.59955 15.25 9.69673H16.75ZM20.4008 12.9703C20.8609 12.6327 21.4218 12.1735 21.8751 11.6493C22.3088 11.1479 22.75 10.4666 22.75 9.69672H21.25C21.25 9.91132 21.1088 10.2424 20.7405 10.6681C20.3919 11.0713 19.9314 11.4542 19.5135 11.7609L20.4008 12.9703ZM22.75 9.69672C22.75 8.59954 22.1875 7.70123 21.2543 7.37409C20.3421 7.05432 19.3057 7.36028 18.4813 8.14963L19.5187 9.23307C20.0442 8.72986 20.5078 8.70189 20.7581 8.78964C20.9874 8.87002 21.25 9.12068 21.25 9.69672H22.75ZM17.5992 12.9703C17.9678 13.2407 18.3816 13.5776 19 13.5776L19 12.0776C18.9756 12.0776 18.9605 12.0775 18.9061 12.0488C18.8202 12.0034 18.7128 11.9269 18.4865 11.7609L17.5992 12.9703ZM19.5135 11.7609C19.2872 11.9269 19.1798 12.0034 19.0939 12.0488C19.0395 12.0775 19.0244 12.0776 19 12.0776L19 13.5776C19.6184 13.5776 20.0322 13.2407 20.4008 12.9703L19.5135 11.7609Z" fill="#1C274C"></path> <path d="M17.9975 18C18 17.8358 18 17.669 18 17.5C18 15.0147 14.4183 13 10 13C5.58172 13 2 15.0147 2 17.5C2 19.9853 2 22 10 22C12.231 22 13.8398 21.8433 15 21.5634" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
-                        Total Sick Leave
+                        <?php 
+                            echo file_get_contents('./assets/sick.svg');
+                        ?>
+                        <div>
+                            <div>12</div>
+                            Total Sick Leave
+                        </div>
                     </div>
                     <div class="dashboard_top_item d-flex align-items-center justify-content-center w-100">
-                        <svg fill="#000000" width="60" viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M500 70q-117 0-217 59-97 57-154 154-59 100-59 217t59 217q57 97 154 154 100 59 217 59t217-59q97-57 154-154 59-100 59-217t-59-217q-57-97-154-154-100-59-217-59zm0 108q79 0 148 36t114.5 99.5T819 455h-69q-12 0-25.5-6.5T704 432l-86-114q-7-10-17.5-10T583 318L417 540q-7 9-17.5 9t-17.5-9l-46-61q-7-10-20.5-17t-24.5-7H181q11-78 56.5-141.5T352 214t148-36zm0 644q-79 0-148-36t-114.5-99.5T181 545h69q12 0 25.5 7t20.5 16l86 115q7 9 17.5 9t17.5-9l166-222q7-10 17.5-10t17.5 10l46 61q7 9 20.5 16t24.5 7h110q-11 78-56.5 141.5T648 786t-148 36z"></path></g></svg>
-                        Pending Status
+                        <?php 
+                            echo file_get_contents('./assets/pending.svg');
+                        ?>
+                        <div>
+                            <div>12</div>
+                            Total Pending Leave
+                        </div>
                     </div>
+                </div>
+                <div class="bg-success py-3 px-3 text-white">
+                    <h4>S. M. Mahin Tazuar</h4>
+                    <div>Software Developer</div>
                 </div>
                 <div class="row dashboard_main_content">
                     <div class="col-lg-4">
@@ -31,52 +50,125 @@
                                     <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Pending Leaves</a>
                                     <a class="nav-item nav-link" id="nav-history-tab" data-toggle="tab" href="#nav-history" role="tab" aria-controls="nav-history" aria-selected="false">Leave History</a>
                                     <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Update Profile</a>
+                                    <a class="nav-item nav-link" id="nav-appeal-tab" data-toggle="tab" href="#nav-appeal" role="tab" aria-controls="nav-appeal" aria-selected="false">Appeal or Edit</a>
                                 </div>
                             </nav>
                         </div>
                     </div>
                     <div class="col-lg-8">
-                        <div class="dashboard_content">
+                        <div class="dashboard_content mt-3">
                             <div class="tab-content" id="nav-tabContent">
                                 <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
                                 <form class="form-container" method="POST">
                                     <div class="form-group">
-                                        <label for="name">Employee Name</label>
-                                        <input type="text" name="name"  class="form-control" placeholder="Write Your Name...">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="name">Employee Position</label>
+                                        <label for="name">Leave Reason</label>
                                         <select class="form-control">
-                                            <option>Software Developer</option>
-                                            <option>Software Engineer</option>
-                                            <option>Designer</option>
-                                            <option>Support</option>
-                                            <option>Content Writer</option>
+                                            <option>Sick Leave</option>
+                                            <option>Casual Leave</option>
+                                            <option>Others</option>
                                         </select>
                                     </div>
                                     <div class="form-group">
-                                        <label for="email">Email address</label>
-                                        <input type="email" name="email"  class="form-control">
+                                        <label for="email">Description</label>
+                                        <textarea type="email" name="email"  class="form-control"></textarea>
                                     </div>
-                                    <div class="form-group">
-                                        <label for="password" >Password</label>
-                                        <input type="password" name="password" class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="password" >Retype Password</label>
-                                        <input type="password" name="password" class="form-control">
+                                    <div class="row">
+                                        <div class="col-lg-6">
+                                            <div class="form-group">
+                                                <label for="email">Start Leave</label>
+                                                <input type="date" name="picker"  class="form-control"/>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <div class="form-group">
+                                                <label for="email">End Leave</label>
+                                                <input type="date" name="picker"  class="form-control"/>
+                                            </div>
+                                        </div>
                                     </div>
                                     <input type="submit" class="btn btn-primary" value="Submit"/>
                                 </form>
                                 </div>
                                 <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-                                    Pending Leave
+                                    <div class="pending-leave-container">
+                                        <div class="pending-leave-header row ">
+                                            <div class="col-lg-2"> Leave Id No  </div>
+                                            <div class="col-lg-2"> Leave Type </div>
+                                            <div class="col-lg-2">Starting Date</div>
+                                            <div class="col-lg-2">Ending Date</div>
+                                            <div class="col-lg-2">Status</div>
+                                            <div class="col-lg-2">Attempt Date</div>
+                                        </div>
+                                        <div class="pending-leave-item row">
+                                            <div class="col-lg-2"> #1  </div>
+                                            <div class="col-lg-2"> Sick </div>
+                                            <div class="col-lg-2">18/11/2000</div>
+                                            <div class="col-lg-2">18/11/2000</div>
+                                            <div class="col-lg-2">Approved</div>
+                                            <div class="col-lg-2">1823</div>
+                                        </div>
+                                        <div class="pending-leave-item row">
+                                            <div class="col-lg-2"> #1  </div>
+                                            <div class="col-lg-2"> Sick </div>
+                                            <div class="col-lg-2">18/11/2000</div>
+                                            <div class="col-lg-2">18/11/2000</div>
+                                            <div class="col-lg-2"> Decline </div>
+                                            <div class="col-lg-2">1823</div>
+                                        </div>
+                                    </div>
                                 </div>
                                 <div class="tab-pane fade" id="nav-history" role="tabpanel" aria-labelledby="nav-history-tab">
                                     Leave History
                                 </div>
                                 <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
                                     Update Profile
+                                </div>
+                                <div class="tab-pane fade" id="nav-appeal" role="tabpanel" aria-labelledby="nav-appeal-tab">
+                                    <form class="form-container" method="POST">
+                                        <div class="form-group">
+                                            <label for="name">Leave Id</label>
+                                            <select class="form-control">
+                                                <option>Leave Id</option>
+                                                <option>1</option>
+                                                <option>2</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="name">Action</label>
+                                            <select class="form-control">
+                                                <option>Select</option>
+                                                <option>Appeal</option>
+                                                <option>Edit</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="name">Leave Reason</label>
+                                            <select class="form-control">
+                                                <option>Sick Leave</option>
+                                                <option>Casual Leave</option>
+                                                <option>Others</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="email">Description</label>
+                                            <textarea type="email" name="email"  class="form-control"></textarea>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-lg-6">
+                                                <div class="form-group">
+                                                    <label for="email">Start Leave</label>
+                                                    <input type="date" name="picker"  class="form-control"/>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-6">
+                                                <div class="form-group">
+                                                    <label for="email">End Leave</label>
+                                                    <input type="date" name="picker"  class="form-control"/>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <input type="submit" class="btn btn-primary" value="Submit"/>
+                                    </form>
                                 </div>
                             </div>
                         </div>
